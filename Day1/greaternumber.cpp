05d07387c85ba21a9b4a00f47ba0985f7e7a016b
Ravi Verma <+>
2024-01-25 00:22:03 +0530

    if-else

---
+++ Day1/greaternumber.cpp
@@ -0,0 +1,15 @@
+ #include<iostream>
+ using namespace std;
+ 
+ int main(){
+    int x,y;
+    cout<<"enter two number ";
+    cin>>x>>y;
+    if(x>y){
+        cout<<"max is x"<<" "<<x;
+    }
+    else{
+        cout<<"max is y "<<" "<<y;
+    }
+    return 0;
+ }
