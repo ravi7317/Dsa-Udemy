a4d160ca95a08e2949a5c23c2db0a117b862b90c
Ravi Verma <+>
2024-01-25 02:51:55 +0530

    switch-case

---
+++ Day1/switch.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    int x=3;
+    
+      switch(x)
+      {
+        case 1: cout<<"One";
+         break;
+         case 2: cout<<"two";
+         default: cout<<"invalid";
+      }
+    return 0;
+}
