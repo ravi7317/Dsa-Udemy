57aa4c397f17a56f0dbb6e19bf50de2c29a7553a
Ravi Verma <+>
2024-02-13 11:41:57 +0530

    size of string

---
+++ Day4/size.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string s1="abcder";
+    cout<<s1.size()<<endl;
+    char ch[20]="dsjfnff";
+    cout<<strlen(ch);
+
+    return 0;
+}
