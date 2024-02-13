15d354231f269f580b9922e7b64524c8b8894299
Ravi Verma <+>
2024-02-13 11:42:26 +0530

    string and sub string

---
+++ Day4/string.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<algorithm>
+using namespace std;
+
+int main(){
+    string str ="hellow ";
+    reverse(str.begin(),str.end());
+    cout<<str;
+    return 0;
+}
---
+++ Day4/substring.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+using namespace std;
+
+int main(){
+    string str ="raviverma";
+    cout<<str.substr(4);
+    return 0;
+}
