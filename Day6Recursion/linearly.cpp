d7dd4ed17bf802f3be0c74d2bc05fa5e137e9f65
Ravi Verma <+>
2024-02-24 03:03:42 +0530

    linear print

---
+++ Day6Recursion/linearly.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void f(int i,int n){
+    if(i>n)
+    return;
+    cout<<i<<" ";
+    f(i+1, n);
+}
+int main(){
+    int n;
+    cout<<"Enter the value ";
+    cin>>n;
+    f(1,n);
+return 0;
+}
